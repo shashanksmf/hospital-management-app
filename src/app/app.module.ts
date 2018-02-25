c030b4d0d0c7b5c51c90f092bc6d4fc9fd0d4335
shashank <shashanksmf@outlook.com>
2018-02-25 23:43:56 +0530

    Login Files added

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule, MatCheckboxModule, MatGridListModule, MatInputModule, MatIconModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Login1Component } from './login1/login1.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'login', component: Login1Component },
+  { path: 'login2', component: LoginComponent },
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    Login1Component,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatInputModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes,{ enableTracing: true })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
