69e97a5cd77833aeb681872c93fc10513ebd1e37
shashank <shashanksmf@outlook.com>
2018-02-26 00:22:27 +0530

    added: Express app added

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.use(express.static(__dirname+ '/dist'))
+
+app.listen(process.env.PORT || 3000);
+
+// Path Location Strategy
+
+app.get('/*',function(req,res){
+  res.sendFile(path.join(__dirname + '/dist/index.html'))
+})
+
+console.log("lisetning");
